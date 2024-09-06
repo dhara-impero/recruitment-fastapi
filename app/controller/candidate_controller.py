e1cc77ca723b3f6cd7ad3c3281af650b79a4a25c
dhara-impero <dhara.impero@gmail.com>
2024-09-06 19:37:39 +0530

    feat: all candidate API

--- app/controller/candidate_controller.py
+++ app/controller/candidate_controller.py
@@ -1,13 +1,16 @@
-from app.config.db_config import user_collection,candidate_collection
+from app.config.db_config import user_collection, candidate_collection
 from app.dtos.user_dto import User
 from app.dtos.candidate_dto import Candidate
 from uuid import uuid4
-from fastapi import HTTPException, status
+from fastapi import HTTPException, status, Query
 import logging
+from bson import ObjectId
+from typing import List, Optional, Literal
+
+
 
 class CandidateController:
     def create_candidate(candidate: Candidate):
-        print("candidate")
         candidate_data = candidate.dict()  # Convert Pydantic model to dict
         try:
             # Insert user data into the MongoDB collection
@@ -30,7 +33,8 @@ class CandidateController:
     
     def get_candidate(candidate_id: str):
         try:
-            candidate_data = CandidateController.candidate_collection.find_one({"_id": candidate_id})
+            print("candidate")
+            candidate_data = candidate_collection.find_one({"_id": ObjectId(candidate_id)})
             if candidate_data is None:
                 raise HTTPException(status_code=404, detail="Candidate not found")
             candidate_data["_id"] = str(candidate_data["_id"])
@@ -39,17 +43,18 @@ class CandidateController:
             logging.error(f"An error occurred while retrieving a candidate: {e}")
             raise HTTPException(status_code=500, detail="Internal Server Error")
 
-    def update_candidate(candidate_id: str, updated_candidate: Candidate):
+    def edit_candidate(candidate_id: str, updated_candidate: Candidate):
+        print("*****")
         try:
-            update_result: UpdateResult = CandidateController.candidate_collection.update_one(
-                {"_id": candidate_id},
+            update_result: UpdateResult = candidate_collection.update_one(
+                {"_id": ObjectId(candidate_id)},
                 {"$set": updated_candidate.dict(exclude_unset=True)}
             )
             if update_result.matched_count == 0:
                 raise HTTPException(status_code=404, detail="Candidate not found")
             if update_result.modified_count == 0:
                 raise HTTPException(status_code=400, detail="Candidate update failed")
-            updated_candidate_data = CandidateController.candidate_collection.find_one({"_id": candidate_id})
+            updated_candidate_data = candidate_collection.find_one({"_id": ObjectId(candidate_id)})
             updated_candidate_data["_id"] = str(updated_candidate_data["_id"])
             return {"status": "success", "candidate": updated_candidate_data}
         except Exception as e:
@@ -58,11 +63,69 @@ class CandidateController:
 
     def delete_candidate(candidate_id: str):
         try:
-            delete_result: DeleteResult = CandidateController.candidate_collection.delete_one({"_id": candidate_id})
+            delete_result: DeleteResult = candidate_collection.delete_one({"_id": ObjectId(candidate_id)})
             if delete_result.deleted_count == 0:
                 raise HTTPException(status_code=404, detail="Candidate not found")
-            return {"status": "success", "detail": "Candidate deleted"}
+            return {"status": "success", "message": "Candidate deleted"}
         except Exception as e:
             logging.error(f"An error occurred while deleting a candidate: {e}")
+            raise HTTPException(status_code=500, detail="Internal Server Error")
+
+    def get_all_candidates(
+        first_name: Optional[str] = Query(None),
+        last_name: Optional[str] = Query(None),
+        email: Optional[str] = Query(None),
+        career_level: Optional[str] = Query(None),
+        job_major: Optional[str] = Query(None),
+        years_of_experience: Optional[int] = Query(None),
+        degree_type: Optional[str] = Query(None),
+        skills: Optional[List[str]] = Query(None),
+        nationality: Optional[str] = Query(None),
+        city: Optional[str] = Query(None),
+        salary_min: Optional[float] = Query(None),
+        salary_max: Optional[float] = Query(None),
+        gender: Optional[Literal['Male', 'Female', 'NotSpecified']] = Query(None),
+        search: Optional[str] = Query(None)
+    ):
+        try:
+            query = {}
+            if first_name:
+                query["first_name"] = first_name
+            if last_name:
+                query["last_name"] = last_name
+            if email:
+                query["email"] = email
+            if career_level:
+                query["career_level"] = career_level
+            if job_major:
+                query["job_major"] = job_major
+            if years_of_experience is not None:
+                query["years_of_experience"] = years_of_experience
+            if degree_type:
+                query["degree_type"] = degree_type
+            if skills:
+                query["skills"] = {"$in": skills}
+            if nationality:
+                query["nationality"] = nationality
+            if city:
+                query["city"] = city
+            if salary_min is not None and salary_max is not None:
+                query["salary"] = {"$gte": salary_min, "$lte": salary_max}
+            elif salary_min is not None:
+                query["salary"] = {"$gte": salary_min}
+            elif salary_max is not None:
+                query["salary"] = {"$lte": salary_max}
+            if gender:
+                query["gender"] = gender
+            if search:
+                query["$text"] = {"$search": search}
+            print("--------------------------------")
+            print(query)
+            candidates = list(candidate_collection.find({"$text": {"$search": "Dhara"}}))
+            for candidate in candidates:
+                candidate["_id"] = str(candidate["_id"])
+            return candidates
+        except Exception as e:
+            logging.error(f"An error occurred while retrieving candidates: {e}")
             raise HTTPException(status_code=500, detail="Internal Server Error")
     
--- app/routes/candidate_routes.py
+++ app/routes/candidate_routes.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from app.dtos.user_dto import User
 from app.dtos.candidate_dto import Candidate
 from app.controller.candidate_controller import CandidateController
+from typing import Optional,List, Literal
 
 candidate = APIRouter()
 
@@ -13,4 +14,44 @@ def create_candidate(candidate: Candidate):
 def get_candidate(candidate_id: str):
     return CandidateController.get_candidate(candidate_id)
 
-# Add more routes for update, delete, etc.
+@candidate.delete("/candidate/{candidate_id}")
+def delete_candidate(candidate_id: str):
+    return CandidateController.delete_candidate(candidate_id)
+
+@candidate.put("/candidate/{candidate_id}")
+def edit_candidate(candidate_id: str,updated_candidate: Candidate):
+    return CandidateController.edit_candidate(candidate_id,updated_candidate)
+
+@candidate.get("/all-candidates")
+def get_all_candidates(
+    first_name: Optional[str] = Query(None),
+    last_name: Optional[str] = Query(None),
+    email: Optional[str] = Query(None),
+    career_level: Optional[str] = Query(None),
+    job_major: Optional[str] = Query(None),
+    years_of_experience: Optional[int] = Query(None),
+    degree_type: Optional[str] = Query(None),
+    skills: Optional[List[str]] = Query(None),
+    nationality: Optional[str] = Query(None),
+    city: Optional[str] = Query(None),
+    salary_min: Optional[float] = Query(None),
+    salary_max: Optional[float] = Query(None),
+    gender: Optional[Literal['Male', 'Female', 'NotSpecified']] = Query(None),
+    search: Optional[str] = Query(None)
+):
+    return CandidateController.get_all_candidates(
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        career_level=career_level,
+        job_major=job_major,
+        years_of_experience=years_of_experience,
+        degree_type=degree_type,
+        skills=skills,
+        nationality=nationality,
+        city=city,
+        salary_min=salary_min,
+        salary_max=salary_max,
+        gender=gender,
+        search=search
+    )
